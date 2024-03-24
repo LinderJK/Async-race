270a77fdf3636018a69a5147f294f6b6e783bdd7
LinderJK <slepchenkovia@gmail.com>
2024-03-24 13:04:33 +0300

    refactor: change styles, rename container

--- async-race/src/app/page/main/main-page.ts
+++ async-race/src/app/page/main/main-page.ts
@@ -3,14 +3,14 @@ import { div, h1, nav } from '../components/BaseComponents';
 
 const mainPage = () => {
     const content = div(
-        'container-fluid p-0 background-body',
+        'container-fluid p-0 app-container',
         div(
             'app-content',
             nav(
                 'app-nav navbar navbar-expand-lg bg-body-black',
                 h1('app-title', 'Async-race')
             ),
-            div('app-garage')
+            div('main')
         )
     );
     return { element: content.getElement(), map: content.getAllChildrenMap() };
