375eb870f06b7092a5421c2668bdedd0145de4e6
LinderJK <slepchenkovia@gmail.com>
2024-03-30 09:13:14 +0300

    feat: add params, engineSwitch method, btn handlers

--- async-race/src/app/components/car/Car.ts
+++ async-race/src/app/components/car/Car.ts
@@ -1,9 +1,11 @@
 import './car.scss';
-import { CarData } from '../../types/data-types';
+import { CarData, CarParams } from '../../types/data-types';
 import { button, div, image, p } from '../../page/components/BaseComponents';
 import CarSvg from '../../../assets/car.svg';
 import FlagSvg from '../../../assets/flag.svg';
-import { IComponent } from '../../types/components-types';
+import { ComponentMap, IComponent } from '../../types/components-types';
+import Loader from '../../services/loader';
+import Button from '../../page/components/button/button';
 
 class Car {
     id: number;
@@ -12,17 +14,57 @@ class Car {
 
     name: string;
 
+    params: CarParams = { velocity: 0, distance: 0 };
+
+    nextEngineStatus: 'started' | 'stopped' = 'started';
+
+    isEngineOn: boolean = false;
+
+    btnStartEngine: Button | undefined = undefined;
+
+    btnStartDrive: Button | undefined = undefined;
+
+    componentMap: ComponentMap;
+
+    carImage: IComponent | undefined;
+
     view: IComponent;
 
     constructor(data: CarData) {
         this.id = data.id;
-        this.view = this.createView();
+
         this.color = data.color;
         this.name = data.name;
+        this.view = this.createView();
+    }
+
+    async engineSwitch() {
+        if (this.nextEngineStatus === 'started') {
+            this.params = await Loader.toggleEngine(
+                this.id,
+                this.nextEngineStatus
+            );
+            this.nextEngineStatus = 'stopped';
+            this.btnStartEngine?.toggleClass('btn-start-engine--sucsess');
+        } else if (this.nextEngineStatus === 'stopped') {
+            this.params = await Loader.toggleEngine(
+                this.id,
+                this.nextEngineStatus
+            );
+            this.nextEngineStatus = 'started';
+            this.btnStartEngine?.toggleClass('btn-start-engine--sucsess');
+        }
     }
 
     // eslint-disable-next-line class-methods-use-this
     createView() {
+        this.btnStartEngine = button('btn-start-engine', 'A', () =>
+            this.engineSwitch()
+        );
+        this.btnStartDrive = button('btn-start-drive', 'B', () => {
+            console.log('start drive');
+            // this.startDrive();
+        });
         const car = div(
             'car',
             div(
@@ -35,8 +77,8 @@ class Car {
                 'car-view my-2',
                 div(
                     'car-view__control',
-                    button('a-btn', 'A', () => {}),
-                    button('b-btn', 'B', () => {})
+                    this.btnStartEngine,
+                    this.btnStartDrive
                 ),
                 div(
                     'car-view__draw',
