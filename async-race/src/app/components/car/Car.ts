0974c606252d44b8b2571b4223a00c51d101b427
LinderJK <slepchenkovia@gmail.com>
2024-04-02 02:00:14 +0300

    refactor: delete console, comments, add JSDoc

--- async-race/src/app/components/car/Car.ts
+++ async-race/src/app/components/car/Car.ts
@@ -9,39 +9,39 @@ import Button from '../../page/components/button/button';
 import ObjectComponent from '../../page/components/object/object';
 
 class Car {
-    id: number;
+    id: number; // Unique identifier for the car.
 
-    color: string;
+    color: string; // Color of the car.
 
-    name: string;
+    name: string; // Name of the car.
 
-    params: CarParams = { velocity: 0, distance: 0 };
+    params: CarParams = { velocity: 0, distance: 0 }; // Parameters related to the car.
 
-    nextEngineStatus: 'started' | 'stopped' = 'started';
+    nextEngineStatus: 'started' | 'stopped' = 'started'; // Indicates the next engine status.
 
-    isEngineOn: boolean = false;
+    isEngineOn: boolean = false; // Indicates whether the engine is currently on.
 
-    btnStartEngine: Button | undefined = undefined;
+    btnStartEngine: Button | undefined = undefined; // Button for starting the engine.
 
-    btnStopEngine: Button | undefined = undefined;
+    btnStopEngine: Button | undefined = undefined; // Button for stopping the engine.
 
-    componentMap: ComponentMap;
+    componentMap: ComponentMap; // Map of components associated with the car view.
 
-    carSvgObj;
+    carSvgObj; // SVG object representing the car image.
 
-    view: IComponent;
+    view: IComponent; // Car view component.
 
-    carImage: IComponent | undefined = undefined;
+    carImage: IComponent | undefined = undefined; // Component representing the car image.
 
-    flagImage: IComponent | undefined = undefined;
+    flagImage: IComponent | undefined = undefined; // Component representing the flag image.
 
-    carContainer: IComponent | undefined = undefined;
+    carContainer: IComponent | undefined = undefined; // The container of the car.
 
-    isAnimation: boolean = false;
+    isAnimation: boolean = false; // Indicates whether the animation is active.
 
-    private timeInRace: number = 0;
+    private timeInRace: number = 0; // Time spent by the car in the race.
 
-    private winsCount: number = 0;
+    private winsCount: number = 0; // Number of wins achieved by the car.
 
     constructor(data: CarData) {
         this.id = data.id;
@@ -52,22 +52,30 @@ class Car {
         this.componentMap = this.view.getAllChildrenMap();
     }
 
+    /**
+     * Creates an SVG object for the cars image.
+     * Sets the color of the SVG based on the cars color property.
+     * @returns {ObjectComponent} The created SVG object component.
+     */
     createSvg() {
         const objectElement = document.createElement('object');
+
         objectElement.setAttribute('type', 'image/svg+xml');
         objectElement.setAttribute('data', `${CarSvg}`);
         objectElement.setAttribute('width', '100%');
         objectElement.setAttribute('height', '100%');
-        // console.log(objectElement);
+
         objectElement.onload = () => {
             const svgDoc = objectElement.contentDocument;
             const path = svgDoc!.getElementById('path3155');
-
             path!.setAttribute('fill', `${this.color}`);
         };
         return new ObjectComponent(objectElement);
     }
 
+    /**
+     * Dispatches a custom event indicating that the car has been selected.
+     */
     select() {
         const selectCarEvent = new CustomEvent('selectCar', {
             detail: { selectedCar: this },
@@ -75,6 +83,10 @@ class Car {
         document.dispatchEvent(selectCarEvent);
     }
 
+    /**
+     * Deletes the car asynchronously.
+     * Dispatches an event indicating that the car has been deleted.
+     */
     async delete() {
         try {
             await Loader.deleteCar(this.id);
@@ -84,6 +96,10 @@ class Car {
         }
     }
 
+    /**
+     * Toggles the engine status of the car asynchronously.
+     * Updates the engine status and UI elements accordingly.
+     */
     async engineSwitch() {
         this.params = await Loader.toggleEngine(this.id, this.nextEngineStatus);
 
@@ -101,15 +117,24 @@ class Car {
         }
     }
 
+    /**
+     * Initiates the drive mode for the car asynchronously.
+     * Animates the cars movement during the drive mode.
+     * @returns {Promise<number>} A promise resolving to the drive status after completion.
+     * If an error occurs, the promise is rejected with the error status.
+     */
     async driveMode() {
         if (!this.isEngineOn) {
             console.error('Need to start engine');
         }
-        console.log('start drive');
+
         this.isAnimation = true;
         await this.animate();
+
         this.btnStartEngine?.setAttributes({ disabled: true });
+
         let driveStatus;
+
         try {
             driveStatus = await Loader.switchToDriveMode(this.id);
         } catch (error) {
@@ -117,10 +142,14 @@ class Car {
             return Promise.reject(driveStatus);
         }
         this.btnStopEngine!.deleteAttribute('disabled');
-        console.log(driveStatus);
+
         return driveStatus;
     }
 
+    /**
+     * Calculates the width of the car container during the race.
+     * @returns {number} The calculated width of the car container.
+     */
     calcContainerRaceWidth(): number {
         let width: number = 0;
         const carContainerWidth = this.carContainer?.getWidth();
@@ -128,6 +157,7 @@ class Car {
         const navCarWidth =
             this.componentMap!.get('car-view__control')?.getWidth() || 0;
         const carImageWidth = this.carImage?.getWidth() || 0;
+
         if (
             typeof carContainerWidth === 'number' &&
             typeof flagImageWidth === 'number'
@@ -137,18 +167,20 @@ class Car {
                 flagImageWidth -
                 navCarWidth -
                 carImageWidth / 2;
-            // console.log(width);
         } else {
             console.error('Error calculating car container width');
         }
-        // console.log(width);
         return width;
     }
 
+    /**
+     * Animates the cars movement during the race.
+     */
     animate() {
         const width = this.calcContainerRaceWidth();
         const start = performance.now();
         const totalTime = this.params.distance / this.params.velocity;
+
         const animateStep = (timestamp: DOMHighResTimeStamp) => {
             const progress = timestamp - start;
             if (!this.isAnimation) {
@@ -160,6 +192,7 @@ class Car {
             this.carImage?.addStyle({
                 transform: `translateX(${distanceMoved}px)`,
             });
+
             if (distanceMoved < width) {
                 requestAnimationFrame(animateStep);
             } else {
@@ -176,11 +209,7 @@ class Car {
             await this.driveMode();
         });
         this.btnStopEngine = button('btn-start-drive', 'B', async () => {
-            // console.log('start drive');
-            // this.driveMode();
-            console.log(this.isEngineOn);
             await this.engineSwitch();
-            console.log(this.isEngineOn);
         });
         this.btnStopEngine.setAttributes({ disabled: true });
 
@@ -200,11 +229,11 @@ class Car {
             'car',
             div(
                 'car__nav',
-                button('nav__select-car button-select', 'Select', () => {
-                    this.select();
+                button('nav__select-car button-select', 'Select', async () => {
+                    await this.select();
                 }),
-                button('nav__remove-car button-remove', 'Remove', () => {
-                    this.delete();
+                button('nav__remove-car button-remove', 'Remove', async () => {
+                    await this.delete();
                 }),
                 p('nav__name-car', `${this.name}`)
             ),
