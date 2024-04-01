3b91cb1888ed9fc7c2fbc55cfb5d35fdcfd0ef26
LinderJK <slepchenkovia@gmail.com>
2024-04-02 01:01:55 +0300

    feat: add drawWinner method

--- async-race/src/app/components/winers/Winners.ts
+++ async-race/src/app/components/winers/Winners.ts
@@ -1,26 +1,53 @@
-import { div, h1, nav } from '../../page/components/BaseComponents';
-import type { PageView } from '../../types/components-types';
+import './winners.scss';
+import { div, h1, p } from '../../page/components/BaseComponents';
+import type { IComponent, PageView } from '../../types/components-types';
+import type { WinnerData } from '../../types/data-types';
+import LoaderWin from '../../services/winner-loader';
 
-class Winners {
+class Winers {
     view: PageView;
+
+    table: IComponent | undefined = undefined;
 
     constructor() {
         this.view = this.createView();
+        this.updateView();
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    async updateView() {
+        this.table?.deleteChildren();
+        const winners: IComponent[] = [];
+        const data = await LoaderWin.getWinners();
+        console.log(data, 'WINNERS');
+        data.forEach((elem: WinnerData) => {
+            console.log(elem);
+            const winner = this.drawWinner(elem);
+            winners.push(winner);
+        });
+        this.table?.appendChildren(winners);
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    drawWinner(data: WinnerData) {
+        const { id, wins, time } = data;
+
+        const tableElem = div(
+            'table-elem',
+            p('elem-text', `${id} ${wins} ${time}`)
+        );
+        return tableElem;
     }
 
     // eslint-disable-next-line class-methods-use-this
     createView() {
+        this.table = div('winners-table');
         const content = div(
-            'container-fluid p-0 app-container',
-            div(
-                'app-content',
-                nav(
-                    'app-nav navbar navbar-expand-lg bg-body-black mx-2',
-                    h1('app-title', 'Async-race')
-                ),
-                div('main')
-            )
+            'winners-container',
+            h1('winners-title', 'Winners'),
+            this.table
         );
+
         return {
             element: content,
             map: content.getAllChildrenMap(),
@@ -28,4 +55,4 @@ class Winners {
     }
 }
 
-export default Winners;
+export default Winers;
